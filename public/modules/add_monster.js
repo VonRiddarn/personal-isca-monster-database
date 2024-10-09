aebd53755441cfad4f6187b9d26f2e215a6835f9
Vonriddarn <vonriddarn@gmail.com>
2024-10-09 02:33:57 +0200

    feat: added form to create a monster - does not function yet

--- public/modules/add_monster.js
+++ public/modules/add_monster.js
@@ -1,7 +1,16 @@
-import { generateDropDownFromEnum, generateNumericInputFieldsFromEnum } from "./utilities.js";
+import { generateDropDownFromEnum, generateNumericInputFieldsFromEnum, generateButton } from "./utilities.js";
 import { MonsterColor, MonsterAttribute } from "./enums.js";
 
+// Find form
 const addMonsterForm = document.getElementById("add-monster").querySelector("form");
 
+// Generate inputs 
 generateDropDownFromEnum(addMonsterForm, MonsterColor, "Color", "add-monster-color");
 generateNumericInputFieldsFromEnum(addMonsterForm, MonsterAttribute, "add-monster-attribute");
+
+// Create submit button
+generateButton(addMonsterForm, "Add monster", "add-monster-form-submit").addEventListener('click', (e) =>
+{
+	e.preventDefault();
+	console.log("RAAAAWR!");
+});
