97abfe74e706e651c05b0281c5b1d69ce10ee230
Vonriddarn <vonriddarn@gmail.com>
2024-10-10 00:00:58 +0200

    feat: collected utilities inside utilities object for cleaner code

--- public/modules/utilities.js
+++ public/modules/utilities.js
@@ -1,107 +1,102 @@
 import { NumericFilterMethod } from "./enums.js";
 
 export {
-	generateDropDownFromEnum,
-	generateRawDropDownFromEnum,
-	generateNumericInputFieldsFromEnum,
-	generateButton,
+	utilities,
 };
 
-function generateDropDownFromEnum(parentElement, enumToItterate, label, idRoot, isFilter = false)
-{
-	// <span>
-	const span = parentElement.appendChild(document.createElement("span"));
-	span.setAttribute("id", `${idRoot}-span`);
+const utilities = {
 
-	// <span> label
-	const labelSpan = span.appendChild(document.createElement("span"));
-
-	// <input type="checkbox">
-	if(isFilter)
-	{
-		const colorCheckBox = labelSpan.appendChild(document.createElement("input"));
-		colorCheckBox.setAttribute("type", "checkbox");
-		colorCheckBox.setAttribute("id", `${idRoot}-isactive`);
-	}
-
-	// <label>
-	const labelElement = labelSpan.appendChild(document.createElement("label"));
-	labelElement.setAttribute("for", `${idRoot}-${isFilter ? "isactive" : "dropdown"}`);
-	labelElement.innerHTML = label;
-
-	// <select>
-	generateRawDropDownFromEnum(span, enumToItterate, `${idRoot}-dropdown`);
-}
-
-/**
- * @param {Element} parentElement HTML element to append the drop down menu to.
- * @param {Object} enumToItterate Enum to extract dropdown values from.
- * @param {String} id HTML element id of this dropdown to reference it later.
- */
-function generateRawDropDownFromEnum(parentElement, enumToItterate, id)
-{
-	// <select>
-	const select = parentElement.appendChild(document.createElement("select"));
-	select.setAttribute("id", id);
-
-	// Add all enum elements
-	// <option value="e"> e </>
-	for (const e in enumToItterate) 
-	{
-		let opt = select.appendChild(document.createElement("option"));
-		opt.setAttribute("value", `${enumToItterate[e]}`);
-		opt.innerHTML = `${enumToItterate[e]}`;
-	}
-}
-
-/**
- * 
- * @param {Element} parentElement The element on where to apply this list of numeric inputs.
- * @param {"Object"} enumToItterate The enum / object from where to get the keys.
- * @param {String} idRoot The root name for the created elements. eg: filtered-search-form
- * @param {Boolean} isFilter Adds a checkbox and numericFilterEnum to the input.
- */
-function generateNumericInputFieldsFromEnum(parentElement, enumToItterate, idRoot, isFilter = false)
-{
-	for (const el in enumToItterate) 
-	{
-		const elementId = `${idRoot}-${el}`;
-
-		// <span> GENERIC
+	generateDropDownFromEnum(parentElement, enumToItterate, label, idRoot, isFilter = false) {
+		// <span>
 		const span = parentElement.appendChild(document.createElement("span"));
-		span.setAttribute("class", `${idRoot}-span`);
+		span.setAttribute("id", `${idRoot}-span`);
 
+		// <span> label
 		const labelSpan = span.appendChild(document.createElement("span"));
 
-		// <input type="checkbox" />
-		if (isFilter)
+		// <input type="checkbox">
+		if(isFilter)
 		{
-			const checkbox = labelSpan.appendChild(document.createElement("input"));
-			checkbox.setAttribute("type", "checkbox");
-			checkbox.setAttribute("id", `${elementId}-isactive`);
+			const colorCheckBox = labelSpan.appendChild(document.createElement("input"));
+			colorCheckBox.setAttribute("type", "checkbox");
+			colorCheckBox.setAttribute("id", `${idRoot}-isactive`);
 		}
 
 		// <label>
-		const label = labelSpan.appendChild(document.createElement("label"));
-		label.setAttribute("for", `${elementId}-${isFilter ? "isactive" : "count"}`);
-		label.innerHTML = `${enumToItterate[el]}`;
+		const labelElement = labelSpan.appendChild(document.createElement("label"));
+		labelElement.setAttribute("for", `${idRoot}-${isFilter ? "isactive" : "dropdown"}`);
+		labelElement.innerHTML = label;
 
 		// <select>
-		if (isFilter)
-			generateRawDropDownFromEnum(labelSpan, NumericFilterMethod, `${elementId}-numeric-filter-dropdown`);
+		generateRawDropDownFromEnum(span, enumToItterate, `${idRoot}-dropdown`);
+	},
 
-		// <input type="number" />
-		const count = span.appendChild(document.createElement("input"));
-		count.setAttribute("type", "number");
-		count.setAttribute("id", `${elementId}-count`);
-	}
-}
+	/**
+	 * @param {Element} parentElement HTML element to append the drop down menu to.
+	 * @param {Object} enumToItterate Enum to extract dropdown values from.
+	 * @param {String} id HTML element id of this dropdown to reference it later.
+	 */
+	generateRawDropDownFromEnum(parentElement, enumToItterate, id) {
+		// <select>
+		const select = parentElement.appendChild(document.createElement("select"));
+		select.setAttribute("id", id);
 
-function generateButton(parentElement, label, id)
-{
-	const btn = parentElement.appendChild(document.createElement("button"));
-	btn.setAttribute("id", id);
-	btn.innerHTML = label;
+		// Add all enum elements
+		// <option value="e"> e </>
+		for (const e in enumToItterate) 
+		{
+			let opt = select.appendChild(document.createElement("option"));
+			opt.setAttribute("value", `${enumToItterate[e]}`);
+			opt.innerHTML = `${enumToItterate[e]}`;
+		}
+	},
+	/**
+	 * 
+	 * @param {Element} parentElement The element on where to apply this list of numeric inputs.
+	 * @param {"Object"} enumToItterate The enum / object from where to get the keys.
+	 * @param {String} idRoot The root name for the created elements. eg: filtered-search-form
+	 * @param {Boolean} isFilter Adds a checkbox and numericFilterEnum to the input.
+	 */
+	generateNumericInputFieldsFromEnum(parentElement, enumToItterate, idRoot, isFilter = false) {
+		for (const el in enumToItterate) 
+		{
+			const elementId = `${idRoot}-${el}`;
 
-	return btn;
+			// <span> GENERIC
+			const span = parentElement.appendChild(document.createElement("span"));
+			span.setAttribute("class", `${idRoot}-span`);
+
+			const labelSpan = span.appendChild(document.createElement("span"));
+
+			// <input type="checkbox" />
+			if (isFilter)
+			{
+				const checkbox = labelSpan.appendChild(document.createElement("input"));
+				checkbox.setAttribute("type", "checkbox");
+				checkbox.setAttribute("id", `${elementId}-isactive`);
+			}
+
+			// <label>
+			const label = labelSpan.appendChild(document.createElement("label"));
+			label.setAttribute("for", `${elementId}-${isFilter ? "isactive" : "count"}`);
+			label.innerHTML = `${enumToItterate[el]}`;
+
+			// <select>
+			if (isFilter)
+				generateRawDropDownFromEnum(labelSpan, NumericFilterMethod, `${elementId}-numeric-filter-dropdown`);
+
+			// <input type="number" />
+			const count = span.appendChild(document.createElement("input"));
+			count.setAttribute("type", "number");
+			count.setAttribute("id", `${elementId}-count`);
+		}
+	},
+
+	generateButton(parentElement, label, id) {
+		const btn = parentElement.appendChild(document.createElement("button"));
+		btn.setAttribute("id", id);
+		btn.innerHTML = label;
+
+		return btn;
+	},
 }
