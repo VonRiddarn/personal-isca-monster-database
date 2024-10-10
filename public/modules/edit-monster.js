2ed3165f3059dcdae58c9cfe64ff3d3e2e6442c5
Vonriddarn <vonriddarn@gmail.com>
2024-10-10 21:52:28 +0200

    feat: added typecheck to monster editor

--- public/modules/edit-monster.js
+++ public/modules/edit-monster.js
@@ -2,7 +2,16 @@ export {
 	openMonsterEditorOnCard,
 };
 
+// Returns true or false depending on if the form could be created.
 function openMonsterEditorOnCard(articleElement, monsterObject)
 {
+	// Type check to get auto complete
+	if(!(articleElement instanceof HTMLElement))
+	{
+		console.warn(`${articleElement} is NOT of type HTMLElement. Aborting!`);
+		return false;
+	}
+
 	
+
 }
