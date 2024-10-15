e06843935bf816216f868209bf08dfac96cf02ca
VonRiddarn <vonriddarn@gmail.com>
2024-10-15 21:58:56 +0200

    feat: added monster generator for object instancing

--- public/modules/add-monster.js
+++ public/modules/add-monster.js
@@ -1,2 +1,34 @@
+import { MonsterAlignment, MonsterAttribute, MonsterColor } from "./enums.js";
+import { monsterList } from "./monster-list.js";
+
+export { monsterGenerator };
+
 // Next monsters Unique Identifier
-let nexyUid = 0;
+let nexyUid = monsterList.getMonsters().at(-1).uid || 0;
+
+const monsterGenerator = 
+{
+	generateMonster()
+	{
+		const monster = 
+		{
+			alias: "Alias",
+        	alignment: MonsterAlignment.Neutral,
+        	color: MonsterColor.Blue,
+        	stats: {},
+      	};
+	
+		monster.alias = document.getElementById("add-monster-alias-textinput").value
+		monster.color = document.getElementById("add-monster-color-dropdown").value
+		monster.alignment = document.getElementById("filtered-search-alignment-dropdown").value
+	
+		for (const attribute in MonsterAttribute) 
+		{
+			let elementId = `add-monster-attribute-${attribute}-count`;
+			monster.stats[attribute] = document.getElementById(`${elementId}`).value;
+		}
+
+		console.log(monster);
+		return monster;
+	},
+};
